545f07232ebb14624c5427a33dcae93f9bc9efc7
clairejamieson <62304480+clairejamieson@users.noreply.github.com>
2020-04-02 09:24:44 +0100

    First version.

---
+++ assets/js/local.js
@@ -0,0 +1,44 @@
+/******************************************************************************/
+function onload ()
+{
+    tipOfTheWeek();
+}
+
+
+/******************************************************************************/
+function tipOfTheWeek ()
+{
+    var tips =
+	[
+	    {
+		header: "Environment",
+		text: `Make sure that you are working in a reasonable environment.  If you’re too hot,
+	               too comfortable, or have just eaten a large meal, you’ll go to sleep.  If
+	               you’re too cold, too uncomfortable, too hungry or there’s too much else going on
+	               around you, you’ll be distracted.`
+	    },
+
+	    {
+		header: "Mental attitude",
+		text: `If you have difficulty remembering stuff &mdash; either when you’re listening
+	               or when you’re reading, try approaching the material as though you were
+	               intending to pick an argument and wanted to disagree violently with everything
+	               being said.  It’s easier to remember things when you have an investment
+	               in them, and this is a simple way of arranging to <em>have</em> an investment.`
+	    },
+
+	    {
+		header: "Physical attitude",
+		text: `Normally when we are interested in things, our physical attitide reflects the
+	               fact &mdash; we sit up and take notice.  It can work the other way round, too
+	               &mdash; sitting forward as though you are interested can help you <em>be</em>
+	               interested, while sitting back as though you’re bored and couldn’t
+	               care less makes it less likely you’ll take things in.`
+	    }
+	];
+
+    var ix = Math.floor(Math.random() * tips.length);
+    $("#tip-of-the-week-header").html("Tip " + (ix + 1) + ": " + tips[ix].header);
+    $("#tip-of-the-week-text").html(tips[ix].text);
+}
+
