0ee90cff49dfc906c500731adbee604001b8d7fa
clairejamieson <62304480+clairejamieson@users.noreply.github.com>
2022-02-12 14:37:27 +0000

    Correct typos and add new entry.

--- assets/js/local.js
+++ assets/js/local.js
@@ -21,7 +21,7 @@ function tipOfTheWeek ()
 	    {
 		header: "Mental attitude",
 		text: `If you have difficulty remembering stuff &mdash; either when you’re listening
-	               or when you’re reading, try approaching the material as though you were
+	               or when you’re reading &mdash; try approaching the material as though you were
 	               intending to pick an argument and wanted to disagree violently with everything
 	               being said.  It’s easier to remember things when you have an investment
 	               in them, and this is a simple way of arranging to <em>have</em> an investment.`
@@ -37,11 +37,19 @@ function tipOfTheWeek ()
 	    },
 		
 	    {
+		header: "Split up your time",
+		text: `Research shows that you recall things best at the start or end of a learning
+		       session.  Take advantage of this by splitting things up so as to create starts
+		       and ends artificially &mdash; several shorter sessions of ten to forty minutes,
+		       with a break of about ten minutes in between, rather than one long session.`
+	    },
+		
+	    {
 		header: "<u>Use</u> what you learn",
 		text: `<u>Do</u> something with the information as you learn it.  Trying to explain
                        it to someone else is one of the best ways of fixing it in your mind and
                        making sure you understand it.  (When my daughter was young, she even taught
-                       her lessons to her rag dolls!)  But if that's no an option, at least take
+                       her lessons to her rag dolls!)  But if that’s not an option, at least take
                        brief notes, even if you don’t intend to do anything with them later.`
 	    }
 	];
